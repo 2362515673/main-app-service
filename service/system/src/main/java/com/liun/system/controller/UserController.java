3dd2550ba81471d228b5f68f1ee2fb93625b802b
LiuNing <2362515673@qq.com>
2025-07-26 23:47:17 +0800

    整合nacos

--- service/system/src/main/java/com/liun/system/controller/UserController.java
+++ service/system/src/main/java/com/liun/system/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.liun.system.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Value("${a}")
+    private String a;
+
     @GetMapping("/hello")
     public String hello() {
         return "hello world";
+    }
+
+    @GetMapping("/a")
+    public String a() {
+        return a;
     }
 }
